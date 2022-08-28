a522b13c2518f5e448d87f4a87014384a2f6efdd
Mikhail K <michael.yk@yandex.ru>
2022-08-28 10:12:18 +0400

    get/set for Wolf

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,22 +1,24 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf1 = new Wolf();
-        wolf1.sex = "Male";
-        wolf1.nickname = "klax";
-        wolf1.age = 7;
-        wolf1.weight = 85.5f;
-        wolf1.color = "gray";
 
-        System.out.println("Sex = " + wolf1.sex);
-        System.out.println("Nickname = " + wolf1.nickname);
-        System.out.println("Age = " + wolf1.age);
-        System.out.println("Weight = " + wolf1.weight);
-        System.out.println("Color = " + wolf1.color);
+        wolf1.setSex("Male");
+        wolf1.setNickname("klax");
+        wolf1.setAge(10);
+        wolf1.setAge(5);
+        wolf1.setWeight(59.8f);
+        wolf1.setColor("grey");        
+        
+        System.out.println(wolf1.getSex());
+        System.out.println(wolf1.getNickname());
+        System.out.println(wolf1.getAge());
+        System.out.println(wolf1.getWeight());
+        System.out.println(wolf1.getColor());
 
-        wolf1.move();
-        wolf1.sit();
-        wolf1.run();
-        wolf1.howl();
-        wolf1.hunt();
+        // wolf1.move();
+        // wolf1.sit();
+        // wolf1.run();
+        // wolf1.howl();
+        // wolf1.hunt();
     }
 }
