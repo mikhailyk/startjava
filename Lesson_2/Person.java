3c4c575f0f550b1091fd989628cd56e21ddafa8f
Mikhail K <michael.yk@yandex.ru>
2022-08-28 07:09:29 +0400

    Add class Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "Male";
+    String name = "Vasja";
+    float height = 1.8f;
+    float weight = 90f;
+    int age = 35;
+
+    void move() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    void speak() {
+
+    }
+    
+    void learnJava() {
+
+    }
+}
